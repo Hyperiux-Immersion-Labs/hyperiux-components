259805388e016e6551a18ce611cf3d1908f420b8
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2026-05-20 19:56:51 +0530

    Update

--- apps/docs/src/app/layout.js
+++ apps/docs/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Space_Grotesk, Inter, Geist_Mono } from "next/font/google";
 import "highlight.js/styles/github-dark.css";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 
 // Display font - for hero headlines (similar to CoinbaseDisplay)
@@ -44,6 +45,7 @@ export default function RootLayout({ children }) {
       >
      
         {children}
+        <Analytics />
       </body>
     </html>
   );
